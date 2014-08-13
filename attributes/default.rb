8ab88936d4f3a1865e1989f079cfcddcebc281a9
Gordon Franke <gordon.franke@sap.com>
2014-08-13 16:25:46 +0200

    add http_port attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['yum']['server']['repo_base_dir'] = '/var/lib/yum-repo'
 default['yum']['server']['http_aliases'] = []
+default['yum']['server']['http_port'] = 80
 default['apache']['include_logrotate'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -20,11 +20,13 @@ end
 # Needs lots of changes to make apache work in Fedora 18/19/20 (apache 2.4)
 # Not going to bother testing older Fedora's since the project moves so fast,
 # anything older is obsolete/unsupported
+node.default['apache']['listen_ports'] << node['yum']['server']['http_port']
 include_recipe 'apache2'
 include_recipe 'apache2::logrotate' if node['apache']['include_logrotate']
 
 web_app 'yum-server' do
   server_name node['hostname']
+  server_port node['yum']['server']['http_port']
   server_aliases [node['fqdn'], node['ipaddress']] + node['yum']['server']['http_aliases']
   docroot node['yum']['server']['repo_base_dir']
   directory_options 'Indexes MultiViews FollowSymlinks'
